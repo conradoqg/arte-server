cfbaa2476da1fde623b10be7dd4c0ab9d9e2c246
Conrado Quilles Gomes <conradoqg@gmail.com>
2018-04-03 22:13:17 -0300

    Improve download link and add copy CLI command to the UI

--- test/httpServer/views/formatters.js
+++ test/httpServer/views/formatters.js
@@ -2,7 +2,7 @@ require('chai').should();
 const formatters = require('../../../lib/httpServer/views/formatters');
 
 describe('Formatters', async () => {
-    it('should build an URL', async () => {        
+    it('should build an URL', async () => {
         const builtURL = formatters.buildURL({
             bucket: 'bucket1',
             name: 'artifact1',
@@ -14,5 +14,45 @@ describe('Formatters', async () => {
         });
 
         builtURL.should.be.equal('/buckets/bucket1/artifacts/artifact1/1.0?os=linux&arch=x86');
-    });    
+    });
+
+    it('should build CLI command for linux/macos', async () => {
+        const req = {
+            headers: {
+                host: 'localhost'
+            },
+            protocol: 'http'
+        };
+        const builtCLICommand = formatters.buildCLICommand(req, {
+            bucket: 'bucket1',
+            name: 'artifact1',
+            version: '1.0',
+            metadata: {
+                os: 'linux',
+                arch: 'x86'
+            }
+        }, 'linux');
+
+        builtCLICommand.should.be.equal('./arte get -b bucket1 -n artifact1 -v 1.0 --metadata.os linux --metadata.arch x86  --url http://localhost');
+    });
+
+    it('should build CLI command for windows', async () => {
+        const req = {
+            headers: {
+                host: 'localhost'
+            },
+            protocol: 'http'
+        };
+        const builtCLICommand = formatters.buildCLICommand(req, {
+            bucket: 'bucket1',
+            name: 'artifact1',
+            version: '1.0',
+            metadata: {
+                os: 'linux',
+                arch: 'x86'
+            }
+        }, 'windows');
+
+        builtCLICommand.should.be.equal('arte.exe get -b bucket1 -n artifact1 -v 1.0 --metadata.os linux --metadata.arch x86  --url http://localhost');
+    });
 });
